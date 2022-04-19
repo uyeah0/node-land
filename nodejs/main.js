69a4c95c69ad8311febbc9ca3a95ba3a01b50ce7
YuYeyeong <79914479+uyeah0@users.noreply.github.com>
2022-04-19 22:19:45 +0900

    [App] 글수정 - 수정된 내용 저장 - #9

--- nodejs/main.js
+++ nodejs/main.js
@@ -119,6 +119,25 @@ var app = http.createServer(function(request,response){
           response.end(template);
         });
       });
+    }else if(pathname === '/update_process') {
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(err){ // data 디렉토리의 있는 예전파일은 id값, title로 이름 바꾸기, function 실행
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
+          })
+          console.log(post);
+       
+      });
     }else{
       response.writeHead(404);
       response.end('Not found');

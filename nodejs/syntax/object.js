f02cd1934b49e3772fc1305d6eab060309b3b932
uyeah0 <s2031@e-mirim.hs.kr>
2022-04-20 23:05:38 +0900

    [JS] 객체의 형식 - #10

---
+++ nodejs/syntax/object.js
@@ -0,0 +1,10 @@
+var members = ['egoing', 'k99-93', 'hoya'];
+console.log(members[1]);
+
+var roles = {
+    'programer':'egoing',
+    'designer':'k8805',
+    'manager':'hoya'
+}
+
+console.log(roles.designer);
